45262fed330b6b5831e10278b57493cea6242f5a
jacov <jacob@stabilityfocus.com>
2020-07-05 11:35:02 -0400

    try disable mfa challenge 2

--- robin_stocks/authentication.py
+++ robin_stocks/authentication.py
@@ -98,7 +98,7 @@ def login(username=None, password=None, expiresIn=86400, scope='internal', by_sm
         'password': password,
         'scope': scope,
         'username': username,
-        'challenge_type': 'none',
+        'mfa_required': 'false',
         'device_token': device_token
     }
     #    #'challenge_type': challenge_type,
